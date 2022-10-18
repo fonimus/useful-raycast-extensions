42fb1417bac3fc41dfc2d186aa5d74617ce5fbf8
fonimus <francois.onimus@gmail.com>
2022-10-18 13:52:44 +0200

    ✨ Add approve action, add possibility to show/hide details, add number of approvals in list/detail

--- github/src/components/pull-requests.tsx
+++ github/src/components/pull-requests.tsx
@@ -1,13 +1,18 @@
-import {useCallback, useState} from "react";
+import {useCallback, useEffect, useState} from "react";
 import {Label, PullRequest} from "@octokit/webhooks-types";
-import {Action, ActionPanel, Icon, List} from "@raycast/api";
+import {Action, ActionPanel, Alert, confirmAlert, Icon, List, showToast, Toast} from "@raycast/api";
 import {useCachedState} from "@raycast/utils";
 import {duration, getOwner, githubClient, repoFromPrefs} from "../utils";
+
+type PullRequestDisplay = PullRequest & {
+    approvals: number
+}
 
 export function PullRequests(props: { repo?: string }) {
     const [isLoading, setIsLoading] = useState(false);
     const [navigation, setNavigation] = useState(props.repo);
-    const [pulls, setPulls] = useState<PullRequest[]>([]);
+    const [pulls, setPulls] = useState<PullRequestDisplay[]>([]);
+    const [withDetails, setWithDetails] = useCachedState<boolean>('with-details', false);
     const [showBot, setShowBot] = useCachedState<boolean>('show-bot', false);
     const [showBody, setShowBody] = useCachedState<boolean>('show-body', false);
 
@@ -17,24 +22,50 @@ export function PullRequests(props: { repo?: string }) {
             .filter(repo => !props.repo || repo.name === props.repo)
             .map(repo => repo.name);
         try {
-            const promises = filteredRepositories.map(repo => githubClient.rest.pulls.list({
+            const listPullsPromises = filteredRepositories.map(repo => githubClient.rest.pulls.list({
                 owner: getOwner(),
                 repo: repo,
                 state: state as "open" | "closed" | "all",
                 page: page,
-                per_page: 50,
-            }).then(value => value.data as PullRequest[]))
-            const results = await Promise.all(promises);
-            setPulls(results
-                .flatMap(value => value)
-                .sort((a, b) => Date.parse(b.created_at) - Date.parse(a.created_at))
-            )
+                per_page: 20,
+            }).then(value => value.data as PullRequestDisplay[]))
+            const allPulls = (await Promise.all(listPullsPromises)).flatMap(value => value)
+            setPulls(allPulls.sort((a, b) => Date.parse(b.created_at) - Date.parse(a.created_at)))
         } catch (e) {
             console.error("Unable to list pull requests for : " + filteredRepositories, e)
         } finally {
             setIsLoading(false)
         }
     }, []);
+
+    const computeApprovals = useCallback(async () => {
+        if (!pulls.length) {
+            return
+        }
+        const approvalPromises = []
+        setIsLoading(true)
+        for (const pull of pulls) {
+            approvalPromises.push(githubClient.rest.pulls.listReviews({
+                owner: getOwner(),
+                repo: pull.head.repo.name,
+                pull_number: pull.number
+            }).then(value => {
+                let approvals = 0
+                for (const review of value.data) {
+                    if (review.state === "APPROVED") {
+                        approvals++;
+                    }
+                }
+                pull.approvals = approvals
+            }));
+        }
+        await Promise.all(approvalPromises).finally(() => setIsLoading(false));
+        setPulls(pulls)
+    }, [pulls]);
+
+    useEffect(() => {
+        computeApprovals()
+    }, [pulls]);
 
     const getPulls = useCallback(() => {
         return pulls.filter((pull) => pull.user.type !== 'Bot' || showBot)
@@ -51,9 +82,65 @@ export function PullRequests(props: { repo?: string }) {
         }
     }, [pulls]);
 
+    const getAccessories = useCallback((pull: PullRequestDisplay) => {
+        const result = [];
+        if (!withDetails) {
+            for (const label of pull.labels) {
+                result.push({text: label.name})
+            }
+            result.push({text: duration(pull.created_at)})
+            if (pull.approvals) {
+                result.push({icon: Icon.Check, text: '' + pull.approvals, tooltip: 'Approved'});
+            }
+        }
+        result.push({text: `#${pull.number}`});
+        return result;
+    }, [withDetails, pulls]);
+
+    const approve = useCallback(async (pull: PullRequestDisplay) => {
+        setIsLoading(true)
+        const toast = await showToast({
+            style: Toast.Style.Animated,
+            title: `Approving #${pull.number}`,
+        });
+
+        try {
+            if (await confirmAlert({
+                title: `Do you really want to approve pull request #${pull.number} ?`,
+                primaryAction: {
+                    title: 'Approve',
+                    style: Alert.ActionStyle.Default
+                },
+                icon: Icon.Check,
+                dismissAction: {
+                    title: 'Cancel',
+                    style: Alert.ActionStyle.Cancel
+                }
+            })) {
+                await githubClient.rest.pulls.createReview({
+                    owner: getOwner(),
+                    repo: pull.head.repo.name,
+                    pull_number: pull.number,
+                    event: 'APPROVE'
+                }).then(value => value.data)
+
+                toast.style = Toast.Style.Success;
+                toast.message = `Approved with success`;
+            } else {
+                await toast.hide()
+            }
+        } catch (error) {
+            console.error('Unable to approve #' + pull.number, error)
+            toast.style = Toast.Style.Failure;
+            toast.message = 'Failed to approve #' + pull.number + '\n' + String(error);
+        } finally {
+            setIsLoading(false);
+        }
+    }, [pulls]);
+
     return <List
         filtering={true}
-        isShowingDetail={getPulls().length > 0}
+        isShowingDetail={withDetails && getPulls().length > 0}
         isLoading={isLoading}
         searchBarPlaceholder={"Search in pull requests"}
         navigationTitle={navigation}
@@ -84,21 +171,29 @@ export function PullRequests(props: { repo?: string }) {
             <List.Item id={`${pull.number}`}
                        key={pull.number}
                        title={pull.title}
-                       accessories={[{text: `#${pull.number}`}]}
+                       keywords={[`${pull.number}`]}
+                       accessories={getAccessories(pull)}
                        actions={
                            <ActionPanel>
                                <ActionPanel.Section title="Navigation">
                                    <Action.OpenInBrowser title="Open in browser"
                                                          shortcut={{modifiers: ["cmd"], key: "o"}}
                                                          url={pull.html_url}/>
-                                   <Action title={showBody ? "Hide body" : "Show body"}
-                                           icon={showBody ? Icon.EyeDisabled : Icon.Eye}
-                                           shortcut={{modifiers: ["cmd"], key: "b"}}
-                                           onAction={() => setShowBody((x) => !x)}/>
+                                   <Action icon={Icon.Check} title="Approve"
+                                           onAction={() => approve(pull)}/>
+                                   <Action title={withDetails ? "Hide details panel" : "Show details panel"}
+                                           icon={withDetails ? Icon.EyeDisabled : Icon.Eye}
+                                           shortcut={{modifiers: ["cmd"], key: "i"}}
+                                           onAction={() => setWithDetails(!withDetails)}/>
+                                   {withDetails && <Action
+                                       title={showBody ? "Hide body" : "Show body"}
+                                       icon={showBody ? Icon.EyeDisabled : Icon.Eye}
+                                       shortcut={{modifiers: ["cmd"], key: "b"}}
+                                       onAction={() => setShowBody(!showBody)}/>}
                                    <Action title={showBot ? "Hide bot PR" : "Show bot PR"}
                                            icon={showBot ? Icon.EyeDisabled : Icon.Eye}
-                                           shortcut={{modifiers: ["cmd"], key: "i"}}
-                                           onAction={() => setShowBot((x) => !x)}/>
+                                           shortcut={{modifiers: ["cmd"], key: "f"}}
+                                           onAction={() => setShowBot(!showBot)}/>
                                </ActionPanel.Section>
                            </ActionPanel>
                        }
@@ -118,6 +213,10 @@ export function PullRequests(props: { repo?: string }) {
                                        <List.Item.Detail.Metadata.Label title="State" text={pull.state}/>
                                        <List.Item.Detail.Metadata.Label title="Creator" text={pull.user.login}
                                                                         icon={pull.user.avatar_url}/>
+                                       {pull.approvals !== undefined && <List.Item.Detail.Metadata.Separator/>}
+                                       {pull.approvals !== undefined &&
+                                           <List.Item.Detail.Metadata.Label title="Approvals"
+                                                                            text={`${pull.approvals}`}/>}
                                        <List.Item.Detail.Metadata.Separator/>
                                        <List.Item.Detail.Metadata.TagList title="Labels">
                                            {pull.labels.map((label: Label) => (
