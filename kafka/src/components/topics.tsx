037ef09741fde6442c34730e51e61e9136ca091e
fonimus <francois.onimus@gmail.com>
2023-02-24 12:36:01 +0100

    feat: add partitions in topics and improve compacted tag

--- kafka/src/components/topics.tsx
+++ kafka/src/components/topics.tsx
@@ -1,6 +1,6 @@
 import { Action, ActionPanel, Color, Icon, List } from "@raycast/api";
 import { useCallback, useEffect, useState } from "react";
-import { ConfigEntries, ConfigResourceTypes } from "kafkajs";
+import { ConfigEntries, ConfigResourceTypes, PartitionMetadata } from "kafkajs";
 import { buildAdmin, getConfig, getEnvs } from "../utils";
 import { useCachedState } from "@raycast/utils";
 
@@ -9,10 +9,37 @@ interface ConfigEntry {
   value: string;
 }
 
+enum Compacted {
+  loading = "loading",
+  unauthorized = "unauthorized",
+  compacted = "compacted",
+  not_compacted = "not_compacted",
+}
+
 interface TopicInfo {
   name: string;
-  compacted?: boolean;
+  compacted: Compacted;
   config: ConfigEntry[];
+  partitions?: number;
+}
+
+function getAccessories(topic: TopicInfo) {
+  const result = [];
+  switch (topic.compacted) {
+    case Compacted.loading:
+      result.push({ tag: { color: Color.Brown, value: topic.compacted } });
+      break;
+    case Compacted.unauthorized:
+      result.push({ tag: { color: Color.Yellow, value: topic.compacted } });
+      break;
+    case Compacted.compacted:
+      result.push({ tag: { color: Color.Green, value: topic.compacted } });
+      break;
+    case Compacted.not_compacted:
+      result.push({ tag: { color: Color.Red, value: topic.compacted } });
+      break;
+  }
+  return result;
 }
 
 export default function KafkaTopics() {
@@ -35,11 +62,26 @@ export default function KafkaTopics() {
           return filterTopics.some((filterTopic) => topic.includes(filterTopic));
         })
         .sort((a, b) => a.localeCompare(b, "en", { sensitivity: "base" }));
-      setTopics(topics.map((topic) => ({ name: topic, config: [] })));
+      setTopics(topics.map((topic) => ({ name: topic, compacted: Compacted.loading, config: [] })));
 
-      const topicInfos: TopicInfo[] = [];
+      const metadata = new Map(
+        (
+          await admin.fetchTopicMetadata({
+            topics: topics,
+          })
+        ).topics.map((topic) => [topic.name, topic.partitions.length])
+      );
+      setTopics(
+        topics.map((topic) => ({
+          name: topic,
+          compacted: Compacted.loading,
+          config: [],
+          partitions: metadata.get(topic),
+        }))
+      );
+
       for (const topic of topics) {
-        let compacted;
+        let compacted: Compacted;
         let config: ConfigEntry[] = [];
 
         try {
@@ -55,22 +97,26 @@ export default function KafkaTopics() {
                 value: configEntry.configValue,
               };
               if (configEntry.configName === "cleanup.policy") {
-                compacted = configEntry.configValue === "compact";
+                compacted = configEntry.configValue === "compact" ? Compacted.compacted : Compacted.not_compacted;
               }
               return entry;
             })
             .sort((a, b) => a.name.localeCompare(b.name));
         } catch (e) {
-          // not authorized
+          compacted = Compacted.unauthorized;
         }
 
-        topicInfos.push({
-          name: topic,
-          compacted,
-          config,
+        setTopics((existingItems) => {
+          return existingItems.map((value) => {
+            const newValue = JSON.parse(JSON.stringify(value));
+            if (newValue.name === topic) {
+              newValue.compacted = compacted;
+              newValue.config = config;
+            }
+            return newValue;
+          });
         });
       }
-      setTopics(topicInfos);
     } catch (e) {
       console.error("Unable to get kafka topics", e);
     } finally {
@@ -101,7 +147,7 @@ export default function KafkaTopics() {
       {topics.map((topic) => (
         <List.Item
           key={topic.name}
-          title={{ value: topic.name, tooltip: topic.name }}
+          title={{ value: topic.name, tooltip: `${topic.name} - ${topic.partitions?.toString()} partitions` }}
           detail={
             <List.Item.Detail
               isLoading={isLoading}
@@ -110,6 +156,9 @@ export default function KafkaTopics() {
                   <List.Item.Detail.Metadata>
                     <List.Item.Detail.Metadata.Label title={"Name"} text={topic.name} />
                     <List.Item.Detail.Metadata.Separator />
+                    <List.Item.Detail.Metadata.Label title={"Partitions"} text={topic.partitions?.toString()} />
+                    <List.Item.Detail.Metadata.Separator />
+                    <List.Item.Detail.Metadata.Label title={"Configurations"} />
                     {topic.config.map((entry) => (
                       <List.Item.Detail.Metadata.Label key={entry.name} title={entry.name} text={entry.value} />
                     ))}
@@ -118,25 +167,7 @@ export default function KafkaTopics() {
               }
             />
           }
-          accessories={
-            topic.compacted !== undefined
-              ? topic.compacted
-                ? [
-                    {
-                      tag: { color: Color.Green, value: "compacted" },
-                    },
-                  ]
-                : [
-                    {
-                      tag: { color: Color.Red, value: "not compacted" },
-                    },
-                  ]
-              : [
-                  {
-                    tag: { color: Color.Yellow, value: "unknown" },
-                  },
-                ]
-          }
+          accessories={getAccessories(topic)}
           actions={
             <ActionPanel>
               <Action.CopyToClipboard content={topic.name} />
